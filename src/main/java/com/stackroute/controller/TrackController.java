d80e548e0859597376381889941b29369b83c32e
aishwarya <pappulaaishwarya@gmail.com>
2019-07-25 11:34:42 +0530

    query-parametr

---
+++ src/main/java/com/stackroute/controller/TrackController.java
@@ -0,0 +1,85 @@
+package com.stackroute.controller;
+
+import com.stackroute.domain.Track;
+import com.stackroute.service.TrackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "api/v1")
+public class TrackController
+{
+    TrackService trackService;
+    @Autowired
+    public TrackController(TrackService userService)
+    {
+        this.trackService=userService;
+
+    }
+    @PostMapping("track")
+    public ResponseEntity<?> saveUser(@RequestBody Track user)
+    {
+      ResponseEntity responseEntity;
+      try
+      {
+          trackService.saveUser(user);
+          responseEntity=new ResponseEntity<String>("successfully completed", HttpStatus.CREATED);
+
+      }catch (Exception ex)
+      {
+          responseEntity=new ResponseEntity<String>(ex.getMessage(),HttpStatus.CONFLICT);
+      }
+
+      return responseEntity;
+    }
+
+    @GetMapping("track")
+    public ResponseEntity<?>getAllUsers()
+    {
+return new ResponseEntity<List<Track>>(trackService.getAllUsers(), HttpStatus.OK);
+    }
+
+
+    // Implementing PUT method
+    @PutMapping("track")
+    public ResponseEntity<?> updateTrack(@RequestBody Track track)
+    {
+        ResponseEntity responseEntity;
+        try{
+            trackService.updateTrack(track);
+            responseEntity=new ResponseEntity("Successfully updated", HttpStatus.CREATED);
+        }
+        catch (Exception e)
+        {
+            responseEntity=new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+        return responseEntity;
+    }
+
+    // Implementing DELETE method
+    @DeleteMapping(value="/track/{id}")
+    public ResponseEntity<?> deleteTrack(@PathVariable("id") int id) {
+        ResponseEntity responseEntity;
+        try {
+            trackService.deleteTrack(id);
+            responseEntity = new ResponseEntity("Successfully deleted", HttpStatus.CREATED);
+        } catch (Exception e) {
+            responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+        return responseEntity;
+    }
+    // Retrieving Data by name field
+    @GetMapping("track/{firstName}")
+    public ResponseEntity<?> trackByname(@PathVariable String firstName)
+    {
+        return new ResponseEntity<>(trackService.trackByName(firstName),HttpStatus.OK);
+    }
+
+
+
+}
---
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -0,0 +1,46 @@
+package com.stackroute.service;
+
+import com.stackroute.domain.Track;
+import com.stackroute.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class TrackServiceImpl implements TrackService {
+    TrackRepository trackRepository;
+
+    @Autowired
+    public TrackServiceImpl(TrackRepository userRepository) {
+        this.trackRepository = userRepository;
+    }
+
+    @Override
+    public Track saveUser(Track user) {
+        Track savedUser = trackRepository.save(user);
+        return savedUser;
+    }
+
+    @Override
+    public List<Track> getAllUsers() {
+        return trackRepository.findAll();
+    }
+
+    @Override
+    public void updateTrack(Track track) {
+        trackRepository.save(track);
+    }
+
+
+    @Override
+    public void deleteTrack(int id) {
+        trackRepository.deleteById(id);
+    }
+    @Override
+    public List<Track> trackByName(String firstName) {
+        return trackRepository.trackByName(firstName);
+    }
+}
+
+
+
