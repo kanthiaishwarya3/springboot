0d8d434e2208c7b3c4b4c8070407652a26582d9a
aishwarya <pappulaaishwarya@gmail.com>
2019-07-25 11:58:13 +0530

    exceptions

---
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -0,0 +1,20 @@
+package com.stackroute.service;
+
+import com.stackroute.domain.Track;
+import com.stackroute.exceptions.TrackAlreadyExistsException;
+import com.stackroute.exceptions.TrackNotFoundException;
+
+import java.util.List;
+// Methods to implement CRUD.
+public interface TrackService
+{
+    public Track saveUser(Track track) throws TrackAlreadyExistsException;
+    public List<Track>getAllUsers();
+    public void deleteTrack(int id);
+    public Track updateTrack(Track track) throws TrackNotFoundException;
+    public Track trackByName(String firstName);
+}
+
+
+
+
